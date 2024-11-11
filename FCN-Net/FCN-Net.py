035ae740689da1a264d6e9a8b4a0d4d2eaca983f
LING <2386399106@qq.com>
2024-11-12 01:44:46 +0800

    FCN-Net

--- FCN-Net/FCN-Net.py
+++ FCN-Net/FCN-Net.py
@@ -2,10 +2,69 @@
 from keras.datasets import mnist
 from keras.utils import to_categorical
 (x_train,y_train),(x_test,y_test)=mnist.load_data()
-print(x_test[1])
+# 归一化
+print("归一化前y_train\n",y_train[0:100],"\n")
+# 独热编码 
+# y_train=to_categorical(y_train,num_classes=10)
+# print("\n归一化前,独热编码y_train:",y_train[0:100],"\n")
 
-# %%
+
+x_train=x_train/255.0
+x_test=x_test/255.0
+#! erro:对标签值归一化后使用to_to_categorical()函数进行独热编码，编码结果是错误的，
+#! issue: to_categorical()函数对缩放后的标签值编码结果与缩放前的结果不一致
+# y_train=y_train/255.0
+# y_test=y_test/255.0
+
+print("\n归一化 y_train:",y_train[0:100],"\n")
+
+y_train=to_categorical(y_train,num_classes=10)
+y_test=to_categorical(y_test,num_classes=10)
+
+print("\n归一化后,独热编码y_train:",y_train[0:100],"\n")
+# print(x_test[1],"\n")
+# print(y_train[0:100])
 import matplotlib.pyplot as plt
 plt.imshow(x_test[1],cmap="gray")
 
 # %%
+from keras.models import Sequential
+from keras.layers import Dense,Flatten
+from keras.optimizers import RMSprop
+
+# %%
+model = Sequential(name="FCN-Net")
+model.add(Flatten(input_shape=(28,28)))
+model.add(Dense(units=64,activation="relu"))
+model.add(Dense(units=32,activation="relu"))
+model.add(Dense(units=32,activation="relu"))
+model.add(Dense(units=10,activation="softmax"))
+model.summary()
+
+# %%
+model.compile(loss='categorical_crossentropy',
+              metrics=['accuracy'],
+              optimizer=RMSprop())
+
+# %%
+model_info=model.fit(x_train,
+          y_train,
+          epochs=10,
+          batch_size=16,
+          validation_split=0.2)
+loss, accuracy = model.evaluate(x_test, y_test)
+print (accuracy)
+
+# %%
+import matplotlib.pyplot as plt
+y=model_info.history["accuracy"]
+print(y)
+plt.xlabel("epoch")
+plt.ylabel("accuracy")
+plt.plot(range(0,len(model_info.history["accuracy"])),y)
+plt.show()
+
+# %%
+
+
+
--- README.md
+++ README.md
@@ -10,16 +10,19 @@
   git clone https://github.com/Perusona/DP-ML-LING.git
   ```
 
-## 计算机视觉
+## 目录
 
-## 机器学习
+1. 计算机视觉
+2. 机器学习
+   1. 全连接神经网络（FCN-Net）
+      1. 独热编码（one-hot encoding）
+3. 深度学习
+4. 数据挖掘
 
-### 全连接神经网络
+## FCN-Net
 
-## 深度学习
+### 独热编码
 
-## 数据挖掘
-
-## 补充
+举例来说，数据集中有$N$个样本，分别为$x^1,x^2,x^3,…,x^N$。每一个样本都有$n$个特征值，对于数据集中的第$i$个样本，其$n$个特征值分别为$x_1^i,x_2^i,x_3^i,…,x_n^i$。所有样本的标签分为$3$类，分别为$c_1 、c_2 、c_3$，可以把这$3$类标签分别表示为$0、1、2$。这样每一个样本的标签都为$0、1、2$中的一个值。
 
 - [VScode快捷键速查表](https://www.cheat-sheet.cn/post/vs-code-keyboard-shortcuts/#:~:text=Visual%20Studio%20Code%E5%BF%AB%E6%8D%B7%E9%94%AE%E9%80%9F%E6%9F%A5%E8%A1%A8)
